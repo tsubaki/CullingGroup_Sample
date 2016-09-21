9ee381742b4773de46db265f3bd3c48fcd585150
tsubaki <ruitomo.ex@gmail.com>
2016-09-21 22:15:36 +0900

    First Commit

---
+++ Assets/Scenes/Sample1/PlaySingleAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Experimental.Director;
+
+[RequireComponent (typeof(Animator))]
+public class PlaySingleAnimation : MonoBehaviour
+{
+	[SerializeField]
+	AnimationClip clip;
+
+	void Start ()
+	{
+		GetComponent<Animator> ().Play (AnimationClipPlayable.Create (clip));
+	}
+}
---
+++ Assets/Scenes/Sample1/Register.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Register : MonoBehaviour
+{
+	void Awake ()
+	{
+		CullingGroupSample.Target target = new CullingGroupSample.Target (gameObject, transform);
+		CullingGroupSample.targets.Add (target);
+	}
+
+	void Start ()
+	{
+		GetComponent<Animator> ().enabled = false;
+	}
+}
